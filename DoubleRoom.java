7797f8cfb6ff54948f336c532d2251273b835fd7
shuvetajovia <shuvetajovi2006a@gmail.com>
2026-03-05 10:57:30 +0530

    Implement Use Case 2: Room abstraction and static availability

---
+++ DoubleRoom.java
@@ -0,0 +1,12 @@
+/**
+ * Represents a Double Room in the hotel.
+ *
+ * @author Student
+ * @version 2.0
+ */
+public class DoubleRoom extends Room {
+
+    public DoubleRoom() {
+        super("Double Room", 2, 350, 180);
+    }
+}
---
+++ SingleRoom.java
@@ -0,0 +1,13 @@
+/**
+ * Represents a Single Room in the hotel.
+ * Inherits common properties from Room class.
+ *
+ * @author Student
+ * @version 2.0
+ */
+public class SingleRoom extends Room {
+
+    public SingleRoom() {
+        super("Single Room", 1, 200, 100);
+    }
+}
